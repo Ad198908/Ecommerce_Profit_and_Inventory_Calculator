14434e0f8b5935cf1659a2044321f23b0b04b5a7
Ad198908 <Agebretensay@student.cscc.edu>
2026-09-14 11:55:34 -0400

    add docstring, tuples, and wellcom message

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+    Program name: Ecommerce_Profit_and_Inventory_Calculator
+    Author: Adhanet Gebretensay
+    Purpose:
+    Date:Septempber 16/2026
+
+    """
+STORE_INFO = ("Admart", "USD")
+
+print("-"*50)
+print(f"WELCOME TO {STORE_INFO[0]}")
+print("1. View Inventory")
+print("2. Update Inventory")
+print("3. Record Sales")
+print("4. View profit")
+print("5. Exit")
+print("-"*50)
+
+
+
+
+
+
+
